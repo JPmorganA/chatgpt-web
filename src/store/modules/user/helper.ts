eaf2922ae0a826a8fb623fb2035792955fc21471
JPmorganA <124872854+JPmorganA@users.noreply.github.com>
2023-05-01 08:16:09 +0800

    更新 helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'http://8.219.115.178:666/app/1.PNG',
+      name: 'AssassinY',
+      description: '<a href="https://www.knjw1.top/" class="text-blue-500" target="_blank" >网盘</a>',
     },
   }
 }
